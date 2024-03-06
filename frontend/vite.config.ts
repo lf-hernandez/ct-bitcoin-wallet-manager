d98c480927b9e3ad7f7f85f7d876b141ef48da79
Luis Felipe Hernandez <17690241+lf-hernandez@users.noreply.github.com>
2024-03-06 14:14:53 -0500

    Bootstrap frontend

---
+++ frontend/vite.config.ts
@@ -0,0 +1,17 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    server: {
+        // Proxy for dev
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8000',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ''),
+            },
+        },
+    },
+});
