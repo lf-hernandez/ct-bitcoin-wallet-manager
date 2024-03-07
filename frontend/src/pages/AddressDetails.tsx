e702f0970991b441ca5a8feaeef227cda868b134
Luis Felipe Hernandez <17690241+lf-hernandez@users.noreply.github.com>
2024-03-06 19:09:21 -0500

    Fix layout

--- frontend/src/pages/AddressDetails.tsx
+++ frontend/src/pages/AddressDetails.tsx
@@ -1,13 +1,7 @@
-import {
-    Box,
-    Button,
-    Container,
-    Divider,
-    List,
-    ListItem,
-    ListItemText,
-    Typography,
-} from '@mui/material';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import DeleteIcon from '@mui/icons-material/Delete';
+import ShowChartIcon from '@mui/icons-material/ShowChart';
+import { Box, Button, Card, CardContent, Container, Divider, Typography } from '@mui/material';
 import { useAddressContext } from '../hooks/address';
 import { useNavigation } from '../hooks/navigation';
 import { Page } from '../types';
@@ -21,61 +15,77 @@ export const AddressDetails = () => {
         return null;
     }
 
+    const iconStyle = { color: 'blue', verticalAlign: 'middle', marginRight: '4px' };
+
     return (
-        <div>
-            <Container maxWidth="lg">
-                <Box
-                    sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}
-                >
-                    <Button variant="contained" onClick={() => setCurrentPage(Page.WALLET)}>
-                        Back
-                    </Button>
-                    <Typography variant="h3" gutterBottom>
-                        Address Details
+        <Container maxWidth="lg">
+            <Box
+                sx={{
+                    my: 4,
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                }}
+            >
+                <Button startIcon={<ArrowBackIcon />} onClick={() => setCurrentPage(Page.WALLET)}>
+                    Back
+                </Button>
+                <Typography variant="h4">Address Details</Typography>
+                <Button startIcon={<DeleteIcon />} variant="contained" color="error">
+                    Delete
+                </Button>
+            </Box>
+            <Card variant="outlined" sx={{ mb: 2 }}>
+                <CardContent sx={{ display: 'flex' }}>
+                    <Typography variant="h6" sx={{ marginRight: 2 }}>
+                        {currentAddress.address}
                     </Typography>
-                    <Button variant="contained" color="error">
-                        Delete
-                    </Button>
-                </Box>
-                <hr />
-                <Typography variant="h6">{currentAddress.address}</Typography>
-                <Typography>Balance: {currentAddress.balance}</Typography>
-                <Typography>
-                    Number of Transactions: {currentAddress.number_of_transactions}
-                </Typography>
-                <Typography>Total Sent: {currentAddress.total_sent}</Typography>
-                <Typography>Total Received: {currentAddress.total_received}</Typography>
-                <Typography variant="subtitle1" gutterBottom>
-                    Transactions (raw data for testing purposes)
-                </Typography>
-                <List>
-                    {currentAddress.raw_data.txs.map((transaction) => (
-                        <div key={transaction.hash}>
-                            <ListItem>
-                                <ListItemText
-                                    primary={`Transaction Hash: ${transaction.hash}`}
-                                    secondary={
-                                        <>
-                                            <Typography
-                                                component="span"
-                                                variant="body2"
-                                                color="textPrimary"
-                                            >
-                                                Balance: {transaction.balance}
-                                            </Typography>
-                                            <br />
-                                            Fee: {transaction.fee}
-                                            <br />
-                                            Result: {transaction.result}
-                                        </>
-                                    }
-                                />
-                            </ListItem>
-                            {currentAddress.raw_data.txs.length > 1 && <Divider />}
-                        </div>
-                    ))}
-                </List>
-            </Container>
-        </div>
+                    <Box sx={{ display: 'flex', alignItems: 'center', mt: 1 }}>
+                        <ShowChartIcon style={iconStyle} />
+                        <Typography variant="body1">Balance: {currentAddress.balance}</Typography>
+                    </Box>
+                    <Box sx={{ display: 'flex', alignItems: 'center', mt: 1 }}>
+                        <ShowChartIcon style={iconStyle} />
+                        <Typography variant="body1">
+                            Transactions: {currentAddress.number_of_transactions}
+                        </Typography>
+                    </Box>
+                    <Box sx={{ display: 'flex', alignItems: 'center', mt: 1 }}>
+                        <ShowChartIcon style={iconStyle} />
+                        <Typography variant="body1">
+                            Total Sent: {currentAddress.total_sent}
+                        </Typography>
+                    </Box>
+                    <Box sx={{ display: 'flex', alignItems: 'center', mt: 1 }}>
+                        <ShowChartIcon style={iconStyle} />
+                        <Typography variant="body1">
+                            Total Received: {currentAddress.total_received}
+                        </Typography>
+                    </Box>
+                </CardContent>
+            </Card>
+            <Typography variant="h5" sx={{ mt: 2, mb: 1 }}>
+                Transactions
+            </Typography>
+            {currentAddress.raw_data.txs.map((transaction, index) => (
+                <Card key={transaction.hash} variant="outlined" sx={{ mb: 2 }}>
+                    <CardContent>
+                        <Typography variant="body1" gutterBottom>
+                            Transaction Hash: {transaction.hash}
+                        </Typography>
+                        <Typography variant="body2" color="textSecondary">
+                            Balance: {transaction.balance}
+                        </Typography>
+                        <Typography variant="body2" color="textSecondary">
+                            Fee: {transaction.fee}
+                        </Typography>
+                        <Typography variant="body2" color="textSecondary">
+                            Result: {transaction.result}
+                        </Typography>
+                    </CardContent>
+                    {currentAddress.raw_data.txs.length > 1 && <Divider />}
+                </Card>
+            ))}
+        </Container>
     );
 };
